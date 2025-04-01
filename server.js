4bc4b7c7bec818a55a098e38a18a352c0681336b
brayanmi129 <141159491+brayanmi129@users.noreply.github.com>
2025-04-01 15:09:01 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 //midelwares
 app.use(express.json());
