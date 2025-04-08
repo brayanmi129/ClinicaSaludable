a15cd73239ae593d36b8c1bbdc1054d6ac5557c6
brayanmi129 <141159491+brayanmi129@users.noreply.github.com>
2025-04-08 12:44:46 -0500

    Delete routes directory

--- routes/usersRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/authRoutes.js
-const express = require("express");
-const router = express.Router();
-const UserManagment = require("../modelo/userManagment"); // Importa el modelo de gestión de usuarios
-
-router.get("/get", UserManagment.getUsers);
-
-module.exports = router; // Exporta el router
