e5603eaff69df998d7fc7b8b84deba40fc9deda3
Brayan Miranda <brakcris129@gmail.com>
2025-04-17 12:31:36 -0500

    change enpoint name

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -6,7 +6,7 @@ router.get("/all", UsersS.getUsers);
 router.get("/id/:id", UsersS.getUsersByID);
 router.get("/email/:email", UsersS.getUsersByEmail);
 router.get("/role/:role", UsersS.getUsersByRole);
-router.post("/update/:id", UsersS.updateUserById);
+router.put("/update/:id", UsersS.updateUserById);
 router.delete("/delete/:id", UsersS.deleteUserById);
 router.post("/register", UsersS.registrerUser);
 
