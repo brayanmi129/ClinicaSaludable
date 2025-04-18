b34a63ff64332b1e09fd9f500eade8a8e56a1b77
Sergio Angel <chechoap@gmail.com>
2025-04-18 14:52:35 -0500

    Update ProtectedRoute.jsx

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -17,9 +17,6 @@ const ProtectedRoute = ({ children }) => {
       try {
         const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/me`, {
             credentials: "include",
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
         });
 
         if (res.ok) {
