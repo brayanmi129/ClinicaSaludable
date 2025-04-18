dacf86f76d87c86f39428731d1abc9548385966f
Sergio Angel <chechoap@gmail.com>
2025-04-18 14:46:37 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,7 @@
 const Home = () => {
-    const token = sessionStorage.getItem('authToken');
-    console.log("token en Home:", token);
-    
     return (
         <div>
         <h1>Bienvenido a la página de inicio</h1>
-        <p>Tu token es: {token}</p>
         </div>
     );
 }
