b77f3560cce33f0f66aedb490f8a7979c84e8c08
Brayan Miranda <brakcris129@gmail.com>
2025-04-17 19:47:42 -0500

    cambios en bcrypt

--- model/authM.js
+++ model/authM.js
@@ -1,7 +1,5 @@
-const crypto = require("crypto");
 const UsersM = require("./userM.js");
-const bcrypt = require("bcrypt");
-
+const bcrypt = require("bcryptjs");
 class AuthM {
   async OAuth(profile) {
     const User = await UsersM.getByEmail(profile.emails[0].value);
--- model/userM.js
+++ model/userM.js
@@ -1,5 +1,5 @@
 const { getConnection, sql } = require("../controler/db");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 class UsersM {
   async getAll() {
